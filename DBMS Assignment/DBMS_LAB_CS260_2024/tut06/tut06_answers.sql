278e496c6c767e4aa4237bd3a079c203550b7d6d
Saumya101203 <118092711+Saumya101203@users.noreply.github.com>
2024-04-16 01:18:38 +0530

    Update tut06_answers.sql

--- DBMS Assignment/DBMS_LAB_CS260_2024/tut06/tut06_answers.sql
+++ DBMS Assignment/DBMS_LAB_CS260_2024/tut06/tut06_answers.sql
@@ -2,6 +2,8 @@
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
 -- Create students table
+create database tut6;
+use tut6
 CREATE TABLE students (
     student_id INT PRIMARY KEY,
     first_name VARCHAR(50),
