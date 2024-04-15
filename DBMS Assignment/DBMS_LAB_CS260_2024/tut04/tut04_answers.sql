ef30f1a221128caa3e71b24989c78f8ff2799db3
Saumya101203 <118092711+Saumya101203@users.noreply.github.com>
2024-04-16 01:21:14 +0530

    Update tut04_answers.sql

--- DBMS Assignment/DBMS_LAB_CS260_2024/tut04/tut04_answers.sql
+++ DBMS Assignment/DBMS_LAB_CS260_2024/tut04/tut04_answers.sql
@@ -1,8 +1,8 @@
 -- General Instructions
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
-create database db2;
-use db2
+create database tut4;
+use tut4
 CREATE TABLE departments (
     department_id INT PRIMARY KEY,
     department_name VARCHAR(50),
