95b2d49c9d151113846c2a8ece18354a76e77eaf
Saumya101203 <118092711+Saumya101203@users.noreply.github.com>
2024-04-16 01:20:05 +0530

    Update tut08_answers.sql

--- DBMS Assignment/DBMS_LAB_CS260_2024/tut08/tut08_answers.sql
+++ DBMS Assignment/DBMS_LAB_CS260_2024/tut08/tut08_answers.sql
@@ -1,7 +1,8 @@
 -- General Instructions
 -- 1.	The .sql files are run automatically, so please ensure that there are no syntax errors in the file. If we are unable to run your file, you get an automatic reduction to 0 marks.
 -- Comment in MYSQL 
-
+create database tut8;
+use tut8
 CREATE TABLE departments (
     department_id INT PRIMARY KEY,
     department_name VARCHAR(50),
